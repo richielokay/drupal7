915d1a57d851216f00c778707a254a57576e3223
Richie Lokay <info@wingtipmarketing.com>
2011-06-16 04:10:40 +0000

    for real

---
+++ sites/all/themes/genesis/genesis_WINGTIP/template.php
@@ -0,0 +1,65 @@
+<?php
+// $Id: template.php,v 1.10 2011/01/14 02:57:57 jmburnz Exp $
+
+
+/**
+ * Override or insert variables into all templates.
+ */
+/* -- Delete this line if you want to use these functions
+function genesis_WINGTIP_preprocess(&$vars, $hook) {
+}
+function genesis_WINGTIP_process(&$vars, $hook) {
+}
+// */
+
+/**
+ * Override or insert variables into the html templates.
+ */
+/* -- Delete this line if you want to use these functions
+function genesis_WINGTIP_preprocess_html(&$vars) {
+  // Uncomment the folowing line to add a conditional stylesheet for IE 7 or less.
+  // drupal_add_css(path_to_theme() . '/css/ie/ie-lte-7.css', array('weight' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
+}
+function genesis_WINGTIP_process_html(&$vars) {
+}
+// */
+
+/**
+ * Override or insert variables into the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function genesis_WINGTIP_preprocess_page(&$vars) {
+}
+function genesis_WINGTIP_process_page(&$vars) {
+}
+// */
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function genesis_WINGTIP_preprocess_node(&$vars) {
+}
+function genesis_WINGTIP_process_node(&$vars) {
+}
+// */
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function genesis_WINGTIP_preprocess_comment(&$vars) {
+}
+function genesis_WINGTIP_process_comment(&$vars) {
+}
+// */
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function genesis_WINGTIP_preprocess_block(&$vars) {
+}
+function genesis_WINGTIP_process_block(&$vars) {
+}
+// */
